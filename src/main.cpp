1fb63fe16bfe0a8f7915575365bb1ad079b032b4
NInjaQuarrior <cmmckevitt@gmail.com>
2022-02-08 15:53:10 -0500

    basic main stuff

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,17 @@ MyDrive drive;
 
 LineSensor lSense;
 
+//START sensor value variables
+float leftSense;
+float rightSense;
+float error;
+
+//ultrasonic
+float curDistIN;
+float curDistCM;
+
+boolean allowRun = true;
+
 void setup()
 {
 
@@ -16,7 +27,31 @@ void setup()
   // put your setup code here, to run once:
 }
 
+void updateValues()
+{
+  leftSense = lSense.getLeft();
+  rightSense = lSense.getRight();
+  error = lSense.getDifference();
+
+  //ultrasonic
+  //curDistIN = ultra.getDistanceIN();
+  //curDistCM = ultra.getDistanceCM();
+}
+
 void loop()
 {
+
+  while (allowRun == true)
+  {
+    if (run(true) == true)
+    {
+      allowRun = false;
+    }
+  }
+
   drive.followLine(lSense.getDifference());
+}
+
+boolean run(boolean side)
+{
 }
